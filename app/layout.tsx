48ffbc24dfb712892b8e81c13ae13e05638ee523
malvaa <malvarez@aluxion.com>
2025-03-18 11:04:36 -0300

    Add: first challenge routes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import './globals.css';
 
+import { MainHeader } from '../components/main-header/main-header.component';
+
 export const metadata = {
   title: 'Next.js Page Routing & Rendering',
   description: 'Learn how to route to different pages.',
@@ -8,7 +10,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
  return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+      <MainHeader/>
+        {children}</body>
     </html>
   )
 }
